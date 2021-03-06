6b2f17da3d1cab69808eda935d2681090b64a84f
Debparna <biswas.debparna.25@gmail.com>
2021-03-07 01:02:27 +0530

    night commit 2

--- src/app/intents.ts
+++ src/app/intents.ts
@@ -59,4 +59,131 @@ export const Intents = {
         "reply": "Sure you are at the right place for this inquiry.",
         "href": "Fee inquiry here"
     },
+    "Banking_Replace_Card":{
+        "reply": "Ok... I can help you replace your card.",
+        "href": "Replace your Card here"
+    },
+    "Banking_Get_Card_Number":{
+        "reply": "Sure, I can help you get your card.",
+        "href": "Get your Card number here"
+    },
+    "Banking_Change_Address":{
+        "reply": "Ok I can help you change your Address.",
+        "href": "Change your Address here"
+    },
+    "Banking_Get_Address":{
+        "reply": "Sorry that you lost track of your address. I can get it for you.",
+        "href": "Find your Address here"
+    },
+    "Banking_Get_Account_Number":{
+        "reply": "Yes, I can get you your account number",
+        "href": "Find your Account number here"
+    },
+    "Banking_Get_Email":{
+        "reply": "Of course.. I can help you find your email id",
+        "href": "Find your Email here"
+    },
+    "Banking_Get_Username":{
+        "reply": "Sure, I can help you find your username",
+        "href": "Find your Username here"
+    },
+    "Banking_Get_Secure_Info":{
+        "reply": "Ok. What kind of security info you need? I can help you.",
+        "href": "Find your Username here"
+    },
+    "Banking_Get_Acc_Status":{
+        "reply": "Ok..I will let you know your account status.",
+        "href": "Get your Account status here"
+    },
+    "Banking_Get_Contact":{
+        "reply": "Opps... Did you loose your contact? I can help you with that",
+        "href": "Get your contact number here"
+    },
+    "Banking_Change_Username":{
+        "reply": "Sure... I can help you change your username..",
+        "href": "Change Username"
+    },
+    "Banking_Change_Credential_Info":{
+        "reply": "All right.!! You sure you want to change your credential information?",
+        "href": "Change Credential Information here."
+    },
+    "Banking_Change_Passcode":{
+        "reply": "All right.!! I shall help you cahnge passcode",
+        "href": "Change Passcode here."
+    },
+    "Banking_Change_Password":{
+        "reply": "All right.!! I shall help you cahnge password",
+        "href": "Change Password here."
+    },
+    "Banking_Change_Contact":{
+        "reply": "Let me guide you to change your contact number.",
+        "href": "Change Contact here."
+    },
+    "Banking_Report_Missing_Card":{
+        "reply": "Opps...So sorry you missed your card.. I will help you..",
+        "href": "Report your Missing card."
+    },
+    "Banking_View_Activity":{
+        "reply": "There are various types of activities to check. I can help you view the specifics.",
+        "href": "Bank Activities"
+    },
+    "Banking_Transfer_Money":{
+        "reply": "All right, I will help you transfer money.",
+        "href": "Transfer Money"
+    },
+    "Bot_Control_Change_Subject":{
+        "reply": "Ok, What else can I help you with?",
+        "href": null
+    },
+    "Bot_Control_Confirm_Presence":{
+        "reply": "I am here. At your service!!",
+        "href": null
+    },
+    "Bot_Control_Standby":{
+        "reply": "Sure... As you wish.",
+        "href": null
+    },
+    "Bot_Control_Start_Over":{
+        "reply": "Okk... Let's start over!!",
+        "href": null
+    },
+    "Banking_Get_Card_Status":{
+        "reply": "Ok.. So you need to check your Card status?",
+        "href": "Track Card status"
+    },
+    "Banking_Get_Account_Details":{
+        "reply": "Sure... I shall help you get your account details..",
+        "href": "Get account details here"
+    },
+    "Banking_Get_Account_Balance":{
+        "reply": "Sure... I will help you check Account balance..",
+        "href": "Check your account balance here"
+    }, 
+    "Banking_Get_Card_Details":{
+        "reply": "Ok.. I will help you view your Card details",
+        "href": "Check your card details here"
+    },
+    "Banking_Card_Request":{
+        "reply": "Sure...I will help to raise request for new Card..",
+        "href": "Generate Card request"
+    },  
+    "Banking_Get_Card_Request_Status":{
+        "reply": "Ok, So you want to track your card request?",
+        "href": "Track your Card request"
+    },  
+    "Bot_Desc":{
+        "reply": "I am your personal assistant for The Generic Bank of the World... \
+         I shall help you solve all your queries and guide you with any online banking activities \
+         You can learn more about the types of us...",
+        "href": "Chat bots are intelligent systems that understand user’s \
+         natural language queries and respond accordingly in a conversation.\
+          In banks, at customer care centers and enquiry desks, \
+          human is insufficient and usually takes long time to process the \
+          single request which results in wastage of time and also reduce quality of customer service. \
+          The primary goal of the chat-bot is, customer can interact with \
+          mentioning their queries in plain English and the chat-bot can resolve their queries \
+          with appropriate response in return."
+    },  
+
+
 }

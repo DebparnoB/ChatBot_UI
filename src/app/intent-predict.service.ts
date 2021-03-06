ca8a606a261926387aa110dad258b9a923494b94
unknown <DebparnoB>
2021-03-06 16:49:20 +0000

    vm commit

--- src/app/intent-predict.service.ts
+++ src/app/intent-predict.service.ts
@@ -17,7 +17,7 @@ export class IntentPredictService {
   constructor(private http: HttpClient) { }
   
   private apiUrl =
-    "http://ec2-3-16-130-112.us-east-2.compute.amazonaws.com:8000/pred_intent/";
+    "http://ec2-52-15-118-138.us-east-2.compute.amazonaws.com:8000/pred_intent/";
 
   getPrediction(message: string): Observable<any> {
     return this.http.get<any>(this.apiUrl + message);
